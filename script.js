5a0f56dec09e57a790ed9bf97bb9f9bcb1dc83ea
rohithk04 <rohithreddy.k0401@gmail.com>
2024-09-10 07:53:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    try {
+        const display = document.getElementById('display');
+        display.value = eval(display.value) || '';
+    } catch {
+        display.value = 'Error';
+    }
+}
